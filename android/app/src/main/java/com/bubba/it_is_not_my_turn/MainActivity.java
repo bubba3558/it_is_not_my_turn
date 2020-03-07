eb4079b6a1514ac9ef3f15a928273dd9da38a08b
bubba3558 <martynaakaniaa@gmail.com>
2020-03-07 20:10:21 +0100

    Initial commit

---
+++ android/app/src/main/java/com/bubba/it_is_not_my_turn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bubba.it_is_not_my_turn;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
