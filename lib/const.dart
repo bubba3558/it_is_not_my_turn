044684ecf586b766e7039f810e3e6fa28ccc8203
bubba3558 <martynaakaniaa@gmail.com>
2020-03-08 22:00:49 +0100

    Add sign out and minor refactor

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,6 @@
 import 'dart:ui';
 
-final themeColor = Color(0xff3279a8);
+final themeColor = Color(0xfffce6c7);
 final primaryColor = Color(0xff203152);
+
+final appTitle = 'It\'s not my turn App';
