665a474dd737e0543ccc2904df51aee6c9cd331c
bubba3558 <martynaakaniaa@gmail.com>
2020-03-14 21:53:07 +0100

    Deactivate completed tasks

--- lib/const.dart
+++ lib/const.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 
 final themeColor = Color(0xfffce6c7);
 final primaryColor = Color(0xff203152);
+final bodyColor = Colors.white;
 final buttonColor = Colors.redAccent;
 final buttonLabelColor = Colors.white;
+final iconButtonColor = Colors.grey;
 
 final appTitle = 'It\'s not my turn App';
